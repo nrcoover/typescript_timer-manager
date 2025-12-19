1b3b95c7b345aa1b948a9bb006361ceacd9c2420
nicklefritz <nrcoover@gmail.com>
2025-12-19 18:05:03 -0500

    Configure dynamic button component with Type Predicates
    Return type of "isAnchorProps" is a type of boolean value based on a predicate. This gives TypeScript extra data on the component type when "href" is present on props.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,21 @@
 import { type ComponentPropsWithoutRef } from "react";
 
-type ButtonProps = {
-	element: "button";
-} & ComponentPropsWithoutRef<"button">;
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+	href?: never;
+};
 
-type AnchorProps = {
-	element: "anchor";
-} & ComponentPropsWithoutRef<"a">;
+type AnchorProps = ComponentPropsWithoutRef<"a"> & {
+	href?: string;
+};
+
+const isAnchorProps = (
+	props: ButtonProps | AnchorProps
+): props is AnchorProps => {
+	return "props" in props;
+};
 
 const Button = (props: ButtonProps | AnchorProps) => {
-	if (props.element === "anchor") {
+	if (isAnchorProps(props)) {
 		return <a className="button" {...props}></a>;
 	}
 
